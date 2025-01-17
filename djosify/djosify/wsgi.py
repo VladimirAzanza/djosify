009d89fbd6be4163d358f78ce2268382b2bfe64d
Vladimir Azanza <vladimirazanza@gmail.com>
2025-01-17 22:15:30 +0300

    Fix small thing

--- djosify/djosify/wsgi.py
+++ djosify/djosify/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djosify.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djosify.djosify.settings')
 
 application = get_wsgi_application()
